5f5a32f2686ebb229ebcfba3bcd342b68171a3e2
UrielIvanBarriosJS <kodovitch.js@gmail.com>
2025-06-17 03:08:21 -0300

    Incorporacion de capa Repository para USuarios

---
+++ src/repository/usuarios.repository.js
@@ -0,0 +1,64 @@
+import supabase from "../config/supabaseClient.js";
+
+export const usuarioRepository = {
+    async obtenerUsuarios(){
+        const { data, errorConsulta } = await supabase.from("usuarios").select("*");
+        if (errorConsulta) throw new Error(`Error al traer los usuarios de la BD: ${errorConsulta.message}`);
+		return data;
+    },
+    async obtenerUsuarioPorEmail(email){
+        const { data, errorConsulta } = await supabase
+            .from("usuarios")
+            .select("*")
+            .eq("email", email)
+            .single();
+            if (errorConsulta && errorConsulta.code !== 'PGRST116') { // PGRST116 es 'No rows found'
+            throw new Error(`Error al buscar usuario por email en la BD: ${errorConsulta.message}`);
+        }   
+        return data;
+    },
+    async crearUsuario(nuevoUsuario){
+        const { data, errorConsulta } = await supabase
+			.from("usuarios")
+			.insert([nuevoUsuario])
+			.select();
+
+		if (errorConsulta) throw new Error(`Error al crear usuario en la BD: ${errorConsulta.message}`);
+		return data[0];
+    },
+    async usuarioExistente(nuevoUsuario){
+        const { data: existentes, error: errorConsulta } = await supabase
+			.from("usuarios")
+			.select("*")
+			.eq("email", nuevoUsuario.email);
+
+		if (errorConsulta) throw new Error(`Error al comprobar la existencia del usuario en la BD: ${errorConsulta.message}`);
+        return existentes;
+    },
+    async obtenerUsuarioPorId(userId){
+        const {data,errorConsulta} = await supabase
+            .from('usuarios')
+            .select('*')
+            .eq('id',userId)
+            .single();
+
+            if(errorConsulta && errorConsulta.code !== 'PGRST116'){
+                throw new Error(`Error al buscar usuario por el Id: ${errorConsulta.message}`)
+            }
+            return data;
+    },
+    async actualizarUsuario(userId, datosNuevos){
+        const{data,errorConsulta} = await supabase
+            .from('usuarios')
+            .update(datosNuevos)
+            .eq('id',userId)
+            .select();
+
+            if(errorConsulta){
+                throw new Error(`Error al actualizar datos del usuario en BD: ${errorConsulta.message}`)
+            }
+
+            return data;
+
+    }
+}
--- src/routes/usuarios.router.js
+++ src/routes/usuarios.router.js
@@ -5,5 +5,7 @@ const usuariosRouter = Router();
 
 usuariosRouter.get("/", usuariosController.obtenerUsuarios);
 usuariosRouter.post("/", usuariosController.crearUsuario);
+usuariosRouter.get("/:id", usuariosController.obtenerUsuarioPorId);
+usuariosRouter.put("/:id", usuariosController.actualizarDatosUsuario);
 
 export { usuariosRouter };
