4fafa5454f4074341332917600065cf4b2d6153e
Brian Isaac Quindi <bquindi@alephee.com>
2025-06-18 20:54:39 -0300

    eliminacion de logs de credenciales

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -7,8 +7,4 @@ const supabase = createClient(
 	process.env.SUPABASE_KEY,
 );
 
-console.log("URL:", process.env.SUPABASE_URL);
-console.log("KEY:", process.env.SUPABASE_KEY);
-console.log("Supabase client initialized");
-
 export default supabase;
